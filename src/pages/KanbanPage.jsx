d69637c6c1f9258d47f256d24153ad7f2bb7d5c7
Kamalesh-git23 <kamalesh23112005@gmail.com>
2026-05-30 19:27:36 +0530

    feat: implement kanban board layout

--- src/pages/KanbanPage.jsx
+++ src/pages/KanbanPage.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import KanbanColumn from '../components/tasks/KanbanColumn';
 
 function KanbanPage() {
+  const {projectId} = useParams();
+
+  const tasks = [
+    {
+      Id:1,
+      title:"Create Navbar",
+      priority: "High",
+      dueDate: "2026-08-20",
+      state:"Todo"
+    },
+
+    {
+      Id:2,
+      title:"Create Sidebar",
+      priority: "Medium",
+      dueDate: "2026-08-21",
+      state:"In Progress"
+    },
+
+    {
+      Id:3,
+      title:"Task API",
+      priority: "Low",
+      dueDate: "2026-08-22",
+      state:"Done"
+    }
+  ];
+
   return (
-    <h1>Kanban Page</h1>
-  )
+    <div className='kanban-page'>
+      <h1>Project:{projectId}</h1>
+
+      <div className='kanban-board'>
+        <KanbanColumn
+          title="Todo"
+          tasks={tasks.filter(task => task.state === "Todo")}/>
+
+        <KanbanColumn
+          title="In Progress"
+          tasks={tasks.filter(task => task.state === "In Progress")}/>
+        
+        <KanbanColumn
+          title="Waiting"
+          tasks={tasks.filter(task => task.state === "Waiting")}/>
+
+        <KanbanColumn
+          title="Done"
+          tasks={tasks.filter(task => task.state === "Done")}/>
+
+      </div>
+
+    </div>
+  );
 }
 
-export default KanbanPage
+export default KanbanPage;
